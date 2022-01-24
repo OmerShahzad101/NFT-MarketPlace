f4ec86a240f84fefa6325530a6e46bcb642b47ed
Ibad Ali <ibad.ali@arhamsoft.org>
2022-01-24 15:25:03 +0500

    createjs

--- src/components/HomeNft/HomeNft.js
+++ src/components/HomeNft/HomeNft.js
@@ -51,7 +51,7 @@ const HomeNft = () => {
             </div>
           </div>
         </div>
-        <div className="row items load-more">
+        {/* <div className="row items load-more">
           {nftData
             ? nftData.map((item, id) => {
                 return (
@@ -101,7 +101,7 @@ const HomeNft = () => {
                 );
               })
             : " "}
-        </div>
+        </div> */}
         <div className="row">
           <div className="col-12 text-center">
             <a id="load-btn" className="btn btn-bordered-white mt-5" href="#">
